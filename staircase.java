b51ee540f968155c050c6f1301d2eec57d801fd1
subrahmanya <96899209+subba5076@users.noreply.github.com>
2023-08-02 12:28:52 +0530

    Create staircase.java

---
+++ staircase.java
@@ -0,0 +1,22 @@
+ublic class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+       bufferedReader.close();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(j<n-1-i){
+                    System.out.print(" ");
+                }else{
+                    System.out.print("#");
+                }
+            }
+            System.out.println("");
+        }
+
+
+        
+    
+}}
