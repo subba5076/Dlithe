85b7f700652890a445084e1580355369fb746b71
subrahmanya <96899209+subba5076@users.noreply.github.com>
2023-08-04 11:58:47 +0530

    Create Reverse_LinkedList.java

---
+++ Reverse_LinkedList.java
@@ -0,0 +1,25 @@
+package temp;
+import java.util.*;
+
+public class temp {  
+    public static void main(String args[]) { 
+   LinkedList<Integer> li = new LinkedList<>();
+   int sum=0;
+   li.add(1);
+   li.add(2);
+   li.add(3);
+   li.add(4);
+   li.add(5);
+   li.add(6);
+   System.out.println("before reversing :"+li);
+Collections.reverse(li);
+System.out.println("after reversing :"+li);
+   
+    }
+    		
+    	}
+       
+         
+
+    
+  
