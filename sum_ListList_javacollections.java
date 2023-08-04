9762f787f4d85a0a1a234974a3a368e8063d8587
subrahmanya <96899209+subba5076@users.noreply.github.com>
2023-08-04 11:48:03 +0530

    Create sum_ListList_javacollections.java

---
+++ sum_ListList_javacollections.java
@@ -0,0 +1,24 @@
+package temp;
+import java.util.*;
+
+public class temp {  
+    public static void main(String args[]) { 
+   LinkedList<Integer> li = new LinkedList<>();
+   int sum=0;
+   li.add(1);
+   li.add(2);
+   li.add(3);
+   li.add(4);
+ for(int x:li) {
+	 sum+=x;
+ }
+ System.out.println(sum);
+   
+    }
+    		
+    	}
+       
+         
+
+    
+  
