c6055fcfea09c5cd0c5233298a2435a53a6d317f
subrahmanya <96899209+subba5076@users.noreply.github.com>
2023-08-04 12:04:21 +0530

    Update cars_count.java

--- cars_count.java
+++ cars_count.java
@@ -1,23 +1,23 @@
 import java.util.*;
-class HelloWorld {
+class temp {
     public static void main(String[] args) {
-      Scanner scr = new Scanner(System.in);
-      int m = scr.nextInt() ;
-      int n = scr.nextInt() ;
+      Scanner sc = new Scanner(System.in);
+      int m = sc.nextInt() ;
+      int n = sc.nextInt() ;
       System.out.println(" Enter the elements");
-      int max = 0,maxi=0;
+      int max = 0,Max=0;
       for(int i =0;i<m;i++){
-          int countc = 0;
+          int counter = 0;
           System.out.println(" Row "+i+1);
          for(int j =0;j<n;j++){
-             int num = scr.nextInt();
-            if(num==1)countc++;
+             int num = sc.nextInt();
+            if(num==1)counter++;
          }
         
-         if(countc>max){
-             max=countc;
-             maxi=i+1;
+         if(counter>max){
+             max=counter;
+             Max=i+1;
          }
     }
-    System.out.println("the row number "+maxi+" has most cars parked: "+max);
+    System.out.println("row "+Max+" has most number of cars parked: "+max);
 }}
